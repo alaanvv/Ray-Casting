7ba8bffea7c4b7df3318fe779d6f4f96064e7147
Alan Vale <valealan84@gmail.com>
2023-11-19 22:30:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,124 @@
+const { PI, cos, sin, tan, sqrt, trunc, round, abs } = Math
 
-const canvas = document.querySelector('canvas')
-canvas.width = 202
-canvas.height = 101
-const ctx = canvas.getContext('2d')
+const FOV = PI * 0.8
+const VIEW_FRAGMENTS = 100
+const VIEW_DENSITY = FOV / VIEW_FRAGMENTS
+const FRAGMENT_WIDTH = canvas.width / VIEW_FRAGMENTS
+const FIX_FISHEYE = 1
+
+const MAX_DISTANCE = 29
+const MAX_WALL_HEIGHT = canvas.height
+
+const ANGLE_STEP = PI / 10
+const MOVE_STEP = 1.49 // More than that may cause teleport trough walls
 
 // ---
 
-function dot(x, y, c) {
-  if (isNaN(x) || isNaN(y)) return console.log('Something is NaN')
-   
-  ctx.fillStyle = c
-  ctx.fillRect(x, y, 1, 1)
+const map = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+const p = { x: 10, y: 10, a: PI * 0 }
+
+// ---
+
+function debug() {
+  document.querySelector('.x').innerText = p.x
+  document.querySelector('.y').innerText = p.y
+  document.querySelector('.a-rad').innerText = p.a.toFixed(2)
+  document.querySelector('.a-deg').innerText = (p.a / PI * 180).toFixed(2)
 }
 
-function line(x1, y1, x2, y2, c) {
-  if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) return console.log('Something is NaN')
-  x1 = Math.round(x1)
-  y1 = Math.round(y1)
-  x2 = Math.round(x2)
-  y2 = Math.round(y2)
+function render() {
+  clear()
+  debug()
 
-  // Reminder: check Bresenham's Line Algorithm
-  const x_size = Math.abs(x1 - x2) + 1
-  const y_size = Math.abs(y1 - y2) + 1
+  for (let i = 0; i < VIEW_FRAGMENTS; i++) {
+    const angle_offset = VIEW_DENSITY * i
+    const angle = p.a + FOV / 2 + VIEW_DENSITY / 2 - angle_offset
+    
+    const d = getDistance(angle) * (FIX_FISHEYE ? abs(cos(angle - p.a)) : 1)
+    
+    const wall_height = (1 - d / MAX_DISTANCE) * MAX_WALL_HEIGHT
+    const wall_brightness = (1 - d / MAX_DISTANCE) * 100
 
-  const x_step = Math.sign(x2 - x1)
-  const y_step = Math.sign(y2 - y1)
-
-  const x_per_y = x_size / y_size
-  const y_per_x = 1 / x_per_y
-
-  if (x_per_y > 1) {
-    for (let x = x1; x !== x2; x += x_step) {
-      const current_x_size = Math.abs(x - x1)
-      dot(x, y1 + Math.round(Math.trunc(current_x_size / x_per_y) * y_step), c)
-    }
-
-    dot(x2, y2, c) // It will not be placed automatically because of the way I did the for condition
+    rect(FRAGMENT_WIDTH * i, (canvas.height - wall_height) / 2, FRAGMENT_WIDTH, wall_height, `rgb(${wall_brightness}, ${wall_brightness}, ${wall_brightness})`)
   }
-  else {
-    for (let y = y1; y !== y2; y += y_step) {
-      const current_y_size = Math.abs(y - y1)
-      dot(x1 + Math.round(Math.trunc(current_y_size / y_per_x) * x_step), y, c)
-    }
+}
 
-    dot(x2, y2, c)
+function getDistance(angle) {
+  let distance = 1
+
+  while (distance <= MAX_DISTANCE) {
+    dx = trunc(cos(angle) * distance)
+    dy = trunc(sin(angle) * distance) * -1
+
+    wx = p.x + dx
+    wy = p.y + dy
+
+    if (map[wy][wx]) return sqrt(dx ** 2 + dy ** 2)
+
+    distance++
   }
 }
 
-function rect(x, y, w, h, c) {
-  if (w === 0 || h === 0) return
-  if (isNaN(x) || isNaN(y) || isNaN(w) || isNaN(h)) return console.log('Something is NaN')
+// ---
 
-  for (let current_x = x; current_x - x < w; current_x++)
-    line(current_x, y, current_x, y + h - 1, c)
-}
+window.addEventListener('keydown', e => {
+  if (!['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'].includes(e.key)) return
 
-function clear() {
-  ctx.fillStyle = 'black'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-}
+  const lock_pos = { x: p.x, y: p.y }
+  
+  switch (e.key) {
+    case 'ArrowLeft':
+      p.a += ANGLE_STEP
+      break
+    case 'ArrowRight':
+      p.a -= ANGLE_STEP
+      break
+    case 'ArrowUp':
+      p.x += cos(p.a) * MOVE_STEP
+      p.y -= sin(p.a) * MOVE_STEP
+      break
+    case 'ArrowDown':
+      p.x -= cos(p.a) * MOVE_STEP
+      p.y += sin(p.a) * MOVE_STEP
+      break
+  }
+
+  p.a %= 2 * PI
+  if (p.a < 0) p.a += 2 * PI
+
+  p.x = round(p.x)
+  p.y = round(p.y)
+
+  if (map[p.y][p.x]) {
+    p.x = lock_pos.x
+    p.y = lock_pos.y
+  }
+
+  render()
+})
+
+// ---
+
+render()
